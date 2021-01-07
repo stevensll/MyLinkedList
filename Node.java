02707927415b54b0025502c2bf78e22b75840fea
Steven <deltaorb637@gmail.com>
2021-01-06 21:30:38 -0500

    Wrote get and set methods for instance variables.

--- Node.java
+++ Node.java
@@ -1,8 +1,26 @@
 public class Node{
-    public Node(String value){
-
-    }
-    private String Data;
+    private String data;
     private Node next, prev;
     
+    public Node(String value){
+        data = value;
+    }
+    public String getData(){
+        return data;
+    }
+    public void setData(String value){
+        data = value;
+    }
+    public Node getNext(){
+        return next;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+    public Node getPrev(){
+        return prev;
+    }
+    public void setPrev(){
+        this.prev = prev;
+    }
 }
